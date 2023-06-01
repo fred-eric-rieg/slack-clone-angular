a4cf451095c1414ec36941900807447135a28cbd
Richard Lieknis <richard.lieknis@web.de>
2023-06-01 19:44:04 +0200

    forgot-pw and signup options added

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  isPasswordVisible: boolean = false;
+  visibiltyIcon: string = 'visibility';
+
+  togglePwVisibility(passwordInput: HTMLInputElement) {
+    this.isPasswordVisible = !this.isPasswordVisible;
+    passwordInput.type = this.isPasswordVisible ? 'text' : 'password';
+    this.visibiltyIcon = this.isPasswordVisible ? 'visibility_off' : 'visibility';
+  }
 
 }
