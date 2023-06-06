abc850c2553b1e2168a73be7b49500667b6b7264
pkieber <113725311+pkieber@users.noreply.github.com>
2023-06-06 17:50:35 +0200

    style user profile

--- src/app/components/dialog-user/dialog-user.component.ts
+++ src/app/components/dialog-user/dialog-user.component.ts
@@ -10,4 +10,8 @@ export class DialogUserComponent {
 
   user: User = new User();
 
+  longText = `The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog
+  from Japan. A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was
+  originally bred for hunting.`;
+
 }
