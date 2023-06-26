983951dce67c013dbc86b8bd80f47443dad81682
Frederic Rieg <62500066+FredVomJupiter@users.noreply.github.com>
2023-06-26 17:48:24 +0200

    implemented user/channel service for dashboard

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,8 @@
 import { Component, HostListener } from '@angular/core';
+import { ChannelService } from 'src/app/shared/services/channel.service';
+import { UserService } from 'src/app/shared/services/user.service';
+import { Channel } from 'src/models/channel.class';
+import { User } from 'src/models/user.class';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,10 +12,36 @@ import { Component, HostListener } from '@angular/core';
 export class DashboardComponent {
 
   positionX: number = 0;
+  channels: Channel[] = [];
+  users: User[] = [];
+  
   
   // Get the mouse position on the dashboard and emit it to the channel component.
   @HostListener('document:mousemove', ['$event'])
   getMousePosition($event: MouseEvent) {
     this.positionX = $event.clientX;
   }
+
+
+  constructor(private channelService: ChannelService, private userService: UserService) {
+    this.loadChannels();
+    this.loadUsers();
+  }
+
+
+  loadChannels() {
+    this.channelService.channels.subscribe((channels: Channel[]) => {
+      this.channels = channels;
+      console.log(this.channels);
+    });
+  }
+
+
+  loadUsers() {
+    this.userService.users.subscribe((users: User[]) => {
+      this.users = users;
+      console.log(this.users);
+    });
+  }
+
 }
