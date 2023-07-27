db3ffda545dcdde4c777434bd37861a84048973d
Frederic Rieg <fredericrieg@protonmail.com>
2023-07-27 19:49:40 +0300

    refactored sidenav with eventEmitter & toggle

--- src/app/shared/services/sidenav.service.ts
+++ src/app/shared/services/sidenav.service.ts
@@ -4,8 +4,12 @@ import { Injectable, EventEmitter } from '@angular/core';
   providedIn: 'root'
 })
 export class SidenavService {
+
+  /**
+  * HIER MUSS NOCH AUFGERÄUMT WERDEN
+  **/
   sidenavOpened = new EventEmitter<boolean>();
-  leftSidenavOpened = new EventEmitter<void>();
+  leftSidenavOpened = new EventEmitter<boolean>();
   isLeftSidenavHidden = false;
 
   constructor() { }
