128e473189d098a016be8626862c911f1e4d0a2e
Frederic Rieg <fredericrieg@protonmail.com>
2023-07-27 19:50:38 +0300

    refactored dashboard for responsive display of components

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { Channel } from 'src/models/channel.class';
+import { SidenavService } from 'src/app/shared/services/sidenav.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,11 +9,28 @@ import { Channel } from 'src/models/channel.class';
 })
 export class DashboardComponent {
 
+  width!: number;
+
+  /**
+   * HostListener listens to the resize event on the window. If innerWidth is less than 768px,
+   * then the left sidenav is hidden automatically. Otherwise the left sidenav is shown (again). 
+   **/
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    if (event.target.innerWidth < 768) {
+      this.width = event.target.innerWidth;
+      this.sidenavService.isLeftSidenavHidden = true;
+    } else {
+      this.width = event.target.innerWidth;
+      this.sidenavService.isLeftSidenavHidden = false;
+    }
+  }
+
   activeChannel!: Channel;
   placeholder!: string;
 
 
-  constructor() { }
+  constructor(public sidenavService: SidenavService) { }
 
 
   setActiveChannel(event: Channel) {
