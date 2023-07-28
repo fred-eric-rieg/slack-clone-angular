48b29f1a0ef12a5e49f68c9463968de2b36b5295
Frederic Rieg <fredericrieg@protonmail.com>
2023-07-28 11:07:26 +0300

    refactored sidenavService in dashboard

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, HostListener, OnDestroy, OnInit } from '@angular/core';
 import { Channel } from 'src/models/channel.class';
 import { SidenavService } from 'src/app/shared/services/sidenav.service';
 
@@ -7,9 +7,10 @@ import { SidenavService } from 'src/app/shared/services/sidenav.service';
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit, OnDestroy {
 
   width!: number;
+  leftSidenavOpen: boolean = true;
 
   /**
    * HostListener listens to the resize event on the window. If innerWidth is less than 768px,
@@ -19,10 +20,10 @@ export class DashboardComponent {
   onResize(event: any) {
     if (event.target.innerWidth < 768) {
       this.width = event.target.innerWidth;
-      this.sidenavService.isLeftSidenavHidden = true;
+      this.sidenavService.openSidenav.emit(false);
     } else {
       this.width = event.target.innerWidth;
-      this.sidenavService.isLeftSidenavHidden = false;
+      this.sidenavService.openSidenav.emit(true);
     }
   }
 
@@ -31,6 +32,18 @@ export class DashboardComponent {
 
 
   constructor(public sidenavService: SidenavService) { }
+
+
+  ngOnInit(): void {
+    this.sidenavService.openSidenav.subscribe((response) => {
+      this.leftSidenavOpen = response;
+    });
+  }
+
+
+  ngOnDestroy(): void {
+    this.sidenavService.openSidenav.unsubscribe();
+  }
 
 
   setActiveChannel(event: Channel) {
