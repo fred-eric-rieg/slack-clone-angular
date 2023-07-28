4e49092036a4617003ee51d35985ece89a374e66
Frederic Rieg <fredericrieg@protonmail.com>
2023-07-28 11:09:11 +0300

    refactored sidenavService & cleaned createChannel method

--- src/app/components/sidenav/sidenav.component.ts
+++ src/app/components/sidenav/sidenav.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { DialogAddChannelComponent } from '../dialog-add-channel/dialog-add-channel.component';
 import { MatDialog } from '@angular/material/dialog';
 import { Channel } from 'src/models/channel.class';
@@ -6,8 +6,7 @@ import { ChannelService } from 'src/app/shared/services/channel.service';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { Timestamp } from '@angular/fire/firestore';
 import { SidenavService } from 'src/app/shared/services/sidenav.service';
-import { ChannelThreadsComponent } from '../channel-threads/channel-threads.component';
-import { RouterModule, Routes } from '@angular/router';
+import { authState } from '@angular/fire/auth';
 
 
 @Component({
@@ -15,14 +14,15 @@ import { RouterModule, Routes } from '@angular/router';
   templateUrl: './sidenav.component.html',
   styleUrls: ['./sidenav.component.scss']
 })
-export class sidenavComponent implements OnInit {
- 
+export class sidenavComponent implements OnInit, OnDestroy {
+
   channelsCollapsed = false;
   messagesCollapsed = false;
 
+  sidenavOpened = true;
+
   channel: Channel = new Channel();
   allChannels!: Array<Channel>;
-  isLeftSidenavHidden = false;
 
 
   constructor(
@@ -34,25 +34,15 @@ export class sidenavComponent implements OnInit {
 
 
   ngOnInit(): void {
-    this.auth.user.subscribe(user => {
-      if (user) {
-        this.channel.creatorId = user.uid;
-        this.channel.members = [user.uid];
-      } else {
-        // This is the default user ID for the guest user.
-        this.channel.creatorId = 'Zta41sUcC7rLGHbpMmn4';
-        this.channel.members = ['Zta41sUcC7rLGHbpMmn4'];
-      }
-    });
     this.loadChannels();
-    /**
-    * Opens and closes the left sidenav.
-    * Checks if sidenav is oppened. If oppened, then the sidenav will be closed after pushing the button in vice-versa.
-    */
-    this.sidenavService.leftSidenavOpened.subscribe(() => {
-      this.sidenavService.isLeftSidenavHidden = !this.sidenavService.isLeftSidenavHidden;
-    });
+    this.handleSidenavVisibility();
   }
+
+
+  ngOnDestroy(): void {
+    this.sidenavService.openSidenav.unsubscribe();
+  }
+
 
   /**
    * Loads all channels from the database once on initialization.
@@ -66,22 +56,42 @@ export class sidenavComponent implements OnInit {
   }
 
 
-  openDialog() {
-    const dialogRef = this.dialog.open(DialogAddChannelComponent);
-
-    dialogRef.afterClosed().subscribe(async (dialogData) => {
-      if (dialogData && dialogData.name) {
-        this.createChannel(dialogData.name);
-      }
+  handleSidenavVisibility() {
+    this.sidenavService.openSidenav.subscribe((response) => {
+      this.sidenavOpened = response;
     });
   }
 
 
+  openDialog() {
+    const dialogRef = this.dialog.open(DialogAddChannelComponent);
+
+    let sub = dialogRef.afterClosed().subscribe(async (dialogData) => {
+      if (dialogData && dialogData.name) {
+        this.createChannel(dialogData.name);
+      }
+    });
+    sub.unsubscribe();
+  }
+
+
   createChannel(dialogData: string) {
+    this.putLoggedUserInNewChannel();
     this.channel.name = dialogData;
     let now = new Date().getTime() / 1000;
     this.channel.creationDate = new Timestamp(now, 0);
     this.channelService.createNewChannel(this.channel);
+  }
+
+
+  putLoggedUserInNewChannel() {
+    let sub = this.auth.authState.subscribe((user) => {
+      user ? (
+        this.channel.creatorId = user.uid,
+        this.channel.members.push(user.uid)
+        ) : null;
+      });
+    sub.unsubscribe();
   }
 
 
