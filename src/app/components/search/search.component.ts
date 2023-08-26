a771fab8ff1fca16af5e56a3b88f4f3e7bb54c2b
pkieber <113725311+pkieber@users.noreply.github.com>
2023-08-26 22:12:17 +0200

    update sidenav-threads

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -14,7 +14,7 @@ export class SearchComponent {
   /**
    * The search-Function is triggered when the user types in the search input field.
    * It emits the 'searchTerm'-value using the 'searchEvent'-emitter.
-   * The parent compoment ('toolbar') can then listen to this event and handle the search functionality accordingly.
+   * The parent component ('toolbar') can then listen to this event and handle the search functionality accordingly.
    */
   search(): void {
     this.searchEvent.emit(this.searchTerm);
--- src/app/components/sidenav-threads/sidenav-threads.component.ts
+++ src/app/components/sidenav-threads/sidenav-threads.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { Subscription, Observable } from 'rxjs';
 import { getAuth } from '@angular/fire/auth';
+import { Firestore, collection, doc, setDoc, query, where, collectionData, getDocs, docData } from '@angular/fire/firestore';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 // Models
 import { Message } from 'src/models/message.class';
@@ -24,12 +26,10 @@ import { EditorChangeContent, EditorChangeSelection } from 'ngx-quill';
 })
 export class SidenavThreadsComponent implements OnInit, OnDestroy {
 
-  activeUser!: User;
+  activeUser!: User; // für theads benötigt
+  collectedContent!: any; // für Editor benötigt
 
-  // Quill editor content
-  collectedContent!: any;
-
-  // Quill editor config
+  // Quill editor config ??
   config = {
     toolbar: [
       ['bold', 'italic', 'underline', 'strike'],
@@ -56,11 +56,12 @@ export class SidenavThreadsComponent implements OnInit, OnDestroy {
   };
 
   placeholder = 'Write a message...';
-
   searchResults!: string[];
-
-  // Subscriptions
   searchSub!: Subscription;
+  threadMessage$!: Observable<any>; // für Threads benötigt
+  allThreadMessages!: Array<any>; // für Threads benötigt
+  userSub!: Subscription;
+  userId: string = '';
 
 
   constructor(
@@ -69,14 +70,14 @@ export class SidenavThreadsComponent implements OnInit, OnDestroy {
     private threadService: ThreadService,
     private messageService: MessageService,
     private channelService: ChannelService,
+    private firestore: Firestore,
+    private auth: AngularFireAuth,
   ) {}
 
 
   ngOnInit(): void {
-    // Loading the logged user
-    this.userService.getSingleUserSnapshot(this.loggedUser()).then((onSnapshot) => {
-      this.activeUser = onSnapshot.data() as User;
-    });
+    this.getLoggedInUser(); // für Threads benötigt
+
     // Search filter (import from searchService)
     this.searchResults = this.searchService.getSearchResults();
     this.searchSub = this.searchService.searchResultsChanged.subscribe((results: string[]) => {
@@ -85,39 +86,50 @@ export class SidenavThreadsComponent implements OnInit, OnDestroy {
   }
 
 
+  // ??
   ngOnDestroy(): void {
     console.log('ThreadComponent destroyed');
   }
 
 
-  async loadThread() {
-   
+  // für Threads benötigt
+  getLoggedInUser() {
+    this.userSub = this.auth.user.subscribe((user: any) => {
+      if (user) {
+        this.userId = user.uid;
+        this.getUser();
+      }
+    });
   }
 
 
-  async loadMessages() {
-    
+  // für Threads benötigt
+  getUser() {
+    this.userService.getSingleUserSnapshot(this.userId).then((onSnapshot) => {
+      this.activeUser = onSnapshot.data() as User;
+      console.log('activeUser fetched:', this.activeUser);
+      this.loadMessagesByActiveUser();
+    });
   }
 
 
-  /**
-   * Returns either the logged user or a default user id.
-   * @returns the logged user id.
-   */
-  loggedUser() {
-    const auth = getAuth();
-    if (auth.currentUser) {
-      return auth.currentUser.uid;
-    } else {
-      return 'Zta41sUcC7rLGHbpMmn4';
-    }
+  // für Threads benötigt
+  async loadMessagesByActiveUser() {
+    const messageCollection = collection(this.firestore, 'messages');
+    const messageQuery = query(messageCollection, where('creatorId', '==', this.activeUser.userId));
+    const messageSnapshot = await getDocs(messageQuery);
+
+    console.log('Active User ID:', this.activeUser.userId);
+
+    this.allThreadMessages = messageSnapshot.docs.map(doc => {
+      const messageData = doc.data();
+      console.log('Message creatorId:', messageData['creatorId']);
+      return messageData;
+    });
   }
 
 
-  sendMessage() {
-
-  }
-
+  // Einstellungen für Editor ??
   /**
    * Filles collectedContent with the current content in the editor.
    * @param event
@@ -125,4 +137,7 @@ export class SidenavThreadsComponent implements OnInit, OnDestroy {
   async collectContent(event: EditorChangeContent | EditorChangeSelection) {
     event.event === 'text-change' ? this.collectedContent = event.html : null;
   }
+
+  sendMessage() {}
+
 }
