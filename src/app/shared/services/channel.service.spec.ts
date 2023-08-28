6e0fdc713d0bd6cf5a352d1f83d679a9d73fecde
Frederic Rieg <fredericrieg@protonmail.com>
2023-08-28 19:48:49 +0300

    some additions for the test

--- src/app/shared/services/channel.service.spec.ts
+++ src/app/shared/services/channel.service.spec.ts
@@ -2,6 +2,22 @@ import { ChannelService } from "./channel.service";
 import { TestBed } from '@angular/core/testing';
 import { DocumentData, DocumentSnapshot, Firestore } from '@angular/fire/firestore';
 
+
+class FirestoreMock {
+    // Hier kannst du Methoden und Eigenschaften des Firestore-Mocks definieren, die du in deinen Tests verwenden möchtest.
+    // Zum Beispiel könntest du Mock-Methoden erstellen, die Daten zurückgeben oder Verhalten simulieren.
+
+    collection(firestore: Firestore, collectionPath: string) {
+        // Hier könntest du eine Mock-Implementierung für die collection-Methode bereitstellen
+        // und eine Dummy-Implementierung eines Firestore-Objekts zurückgeben.
+        return {
+            // Hier könntest du weitere Methoden oder Eigenschaften des Firestore-Objekts simulieren
+        };
+    }
+    
+    // Weitere Mock-Methoden hier
+}
+
 describe('ChannelService', () => { 
     let service: ChannelService;
 
@@ -9,7 +25,7 @@ describe('ChannelService', () => {
         TestBed.configureTestingModule({
             providers: [
                 ChannelService,
-                { provide: Firestore, useValue: { /* Mock implementation if needed */ } }
+                { provide: Firestore, useClass: { FirestoreMock} }
             ]
         });
         service = TestBed.inject(ChannelService);
